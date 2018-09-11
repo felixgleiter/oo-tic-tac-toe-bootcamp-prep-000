28e0a6eaea6a9e6a9abac888d0264e69f1ac122c
Felix Godfree <felixfgodfree@gmail.com>
2018-09-11 17:19:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
     [2,4,6]
   ]
   
-  def display_board(board)
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -39,12 +39,12 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
-    if valid_move?(board, index)
-      player_move(board, index, current_player(board))
+    if valid_move?(index)
+      move(index, current_player(board))
       display_board(board)
     else 
       puts "That is not a valid move, please try again."
